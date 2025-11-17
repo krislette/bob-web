0cfe7f6fad694a6b8102a952ef76e51432e5db1c
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-11-17 16:18:07 +0800

    Fixed display of whole values to rounded off version

--- src/components/ResultsMain.tsx
+++ src/components/ResultsMain.tsx
@@ -24,7 +24,8 @@ function ResultsMain({ prediction, explanation, fileName }: ResultsMainProps) {
   //   prediction?.confidence || 0,
   //   97
   // );
-  const confidenceScore = prediction?.confidence || 0;
+  // NOTE: Remove Math.floor if adjusting is to be restored
+  const confidenceScore = Math.floor(prediction?.confidence) || 0;
   const classification = prediction?.label || "Unknown";
 
   const handleAnalyze = () => {
